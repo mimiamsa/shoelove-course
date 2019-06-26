5d0ec2144b67f080eba81710deb0c4eae6747680
meta-one <meta-one@meta-one.local>
2019-06-26 18:45:27 +0200

    moved routes to routes folder

--- server.js
+++ server.js
@@ -5,37 +5,19 @@ const express = require("express");
 const hbs = require("hbs");
 const app = express();
 
-app.locals.site_url = process.env.SITE_URL; // used in front end to perform ajax request on a url var instead of hardcoding it
+app.locals.site_url = process.env.SITE_URL;
+// used in front end to perform ajax request on a url var instead of hardcoding it
 
 app.set("view engine", "hbs"); //
 app.set("views", __dirname + "/views"); //
 app.use(express.static("public"));
 hbs.registerPartials(__dirname + "/views/partials");
 
-app.get(["/", "/home"], (req, res) => { // clbk parameters are mandatory
-  res.render("index");
-});
+const basePageRouter = require("./routes/index");
+const adminRouter = require("./routes/admin");
 
-app.get("/prod-add", (req, res) => { // clbk parameters are mandatory
-  // res.send("<h1>hello world</h1>");
-  res.render("products_add");
-});
-app.get("/prod-manage", (req, res) => { // clbk parameters are mandatory
-  res.render("products_manage");
-});
-
-app.get("/signup", (req, res) => { // clbk parameters are mandatory
-  res.render("signup");
-});
-
-app.get("/login", (req, res) => { // clbk parameters are mandatory
-  res.render("login");
-});
-
-app.get("/one-product", (req, res) => { // clbk parameters are mandatory
-  res.render("one_product");
-});
-
+app.use(basePageRouter);
+app.use(adminRouter);
 
 const listener = app.listen(process.env.PORT || 8000, () => {
   console.log(
