8ab9738271141987923f7c6ee6fa9abdb51d353f
meta-one <meta-one@meta-one.local>
2019-06-28 11:05:14 +0200

    modified server.js : added session and cookie and needed libs + custom middleware

--- server.js
+++ server.js
@@ -4,19 +4,56 @@ require("./config/db_connection"); // database initial setup
 const express = require("express");
 const hbs = require("hbs");
 const app = express();
+const session = require("express-session");
+const mongoose = require("mongoose");
+const MongoStore = require("connect-mongo")(session);
+const cookieParser = require("cookie-parser");
 
-app.locals.site_url = process.env.SITE_URL;
-// used in front end to perform ajax request on a url var instead of hardcoding it
-
-app.set("view engine", "hbs"); //
-app.set("views", __dirname + "/views"); //
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
 app.use(express.static("public"));
 hbs.registerPartials(__dirname + "/views/partials");
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cookieParser());
+
+// SESSION SETUP
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 60000 }, // in millisec
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    }),
+    saveUninitialized: true,
+    resave: true
+  })
+);
+
+app.locals.site_url = process.env.SITE_URL;
+// used in front end to perform ajax request (var instead of hardcoded)
+
+// CUSTOM MIDDLEWARE
+// check if user is logged in... 
+// usecases : conditional display in hbs templates
+// this function must be declared before the session setup
+// this function must be declared before app.use(router(s))
+function checkloginStatus(req, res, next) {
+  // console.log(req);
+  // res.locals.user = req.user; // get {{user}} in .hbs
+  next();
+}
+
+app.use(checkloginStatus);
+
 const basePageRouter = require("./routes/index");
+const authRouter = require("./routes/auth");
 const adminRouter = require("./routes/admin");
 
 app.use(basePageRouter);
+app.use(authRouter);
 app.use(adminRouter);
 
 const listener = app.listen(process.env.PORT || 8000, () => {
