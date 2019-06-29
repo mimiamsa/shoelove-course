c674fa0035f8a0e7fc35dc484c56b86e6e89c131
meta-one <meta-one@meta-one.local>
2019-06-29 12:17:18 +0200

    :)

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const router = new express.Router();
+const sneakerApi = require("../api/sneaker");
+const tagApi = require("../api/tag");
 
 router.get(["/", "/home"], (req, res) => {
   res.render("index");
 });
 
-router.get(["/collection", "/kids", "/women", "/men"], (req, res) => {
-  res.render("products");
+router.get("/sneakers/:cat", (req, res) => {
+  Promise.all([sneakerApi.getAll(req.params.cat), tagApi.getAll()])
+  .then(apiRes => {
+    // console.log(apiRes);
+    res.render("products", {
+      scripts: ["ajax_tag_filter.js"],
+      sneakers: apiRes[0],
+      tags: apiRes[1],
+    })
+  })
+  .catch(dbErr => {
+    console.log(dbErr);
+    throw new Error(dbErr)
+  })
+});
+
+router.get("/one-product/:id", (req, res) => {
+  sneakerApi.getOne(req.params.id)
+  .then(apiRes => {
+    res.render("one_product", {sneaker: apiRes});
+  })
+  .catch(apiErr => {
+    console.log(apiErr)
+  })
 });
 
 router.get("/signup", (req, res) => {
@@ -15,10 +39,6 @@ router.get("/signup", (req, res) => {
 
 router.get("/signin", (req, res) => {
   res.render("signin", {msg: req.session.msg});
-});
-
-router.get("/one-product", (req, res) => {
-  res.render("one_product");
 });
 
 module.exports = router;
