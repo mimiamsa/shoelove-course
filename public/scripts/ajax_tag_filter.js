bf0f553d89194070f26f891abd6082a0f24b083b
meta-one <meta-one@meta-one.local>
2019-06-29 16:42:55 +0200

    finished ajax filter tag on input change :)

--- public/scripts/ajax_tag_filter.js
+++ public/scripts/ajax_tag_filter.js
@@ -1,13 +1,48 @@
 import apiSneakersHandler from "./api_handlers/sneaker.js";
 
-const links = document.querySelectorAll(".category-list-item");
+const productGrid = document.getElementById("products_grid");
+const tagList = document.getElementById("tag_list");
+const productsCount = document.getElementById("products_count");
+const inputs = document.querySelectorAll("#tag_list input");
 
-links.forEach(link => {
-    link.onclick = function(evt) {
-        evt.preventDefault();
-        apiSneakersHandler.getProductsByTagId(evt.target.getAttribute("data-tag-id"))
-        .then(apiRes => {
-            console.log(apiRes)
-        })
-    }
+var currentCategory;
+
+if (tagList) currentCategory = tagList.getAttribute("data-category");
+
+function drawSneakers(list) {
+  function render(s) {
+    return `<a href="/one-product/${s._id}" class="product-item-wrapper">
+        <div class="product-img">
+            <img src="${s.image}" alt="${s.name} : what a nice pair of kicks">
+        </div>
+        <p class="product-name">${s.name}</p>
+        <p class="product-cat">${s.category}</p>
+        <p class="product-price">${s.price}</p>
+    </a>`;
+  }
+
+  list.forEach(sneaker => {
+    productGrid.innerHTML += render(sneaker);
+  });
+}
+
+inputs.forEach(input => {
+  input.onchange = function(evt) {
+    const checkedInputs = document.querySelectorAll("#tag_list input:checked");
+    const tagIds = [...checkedInputs].map(input =>
+      input.getAttribute("data-tag-id")
+    );
+
+    apiSneakersHandler
+      .getProductsByTagId(tagIds, currentCategory)
+      .then(apiRes => {
+        console.log(apiRes.data);
+
+        productGrid.innerHTML = "";
+        productsCount.textContent = apiRes.data.length;
+        if (apiRes.data.length) drawSneakers(apiRes.data);
+        else
+          productGrid.innerHTML = "<p>sorry, no match for these criteria</p>";
+      });
+  };
 });
