6b8ce595982c09fd07bc0c19d83c652df4370d73
meta-one <meta-one@meta-one.local>
2019-06-29 12:26:34 +0200

    added post routes for edit/add prod

--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -1,35 +1,93 @@
 const express = require("express"); // import express in this module
 const router = new express.Router(); // create an app sub-module (router)
-const sneakerApi = require("../api/api_sneaker");
-
+const sneakerApi = require("../api/sneaker");
+const tagApi = require("../api/tag");
+const uploader = require("./../config/cloudinary.js");
 
 router.get("/prod-add", (req, res) => {
-  res.render("products_add");
+  tagApi
+    .getAll()
+    .then(apiRes => {
+      res.render("products_add", {
+        tags: apiRes,
+        msg: req.session.msg,
+        scripts: ["ajax_tag_form.js"]
+      });
+      req.session.msg = null;
+    })
+    .catch(err => console.log(err));
 });
 
-router.post("/prod-add", (req, res) => {
+router.post("/prod-add", uploader.single("image"), (req, res) => {
   // return console.log(req.body)
-  const { name, ref, size, description, price, tags } = req.body
-  sneakerApi.create({ name, ref, size, description, price, tags })
-  .then(dbRes => {
-    console.log(dbRes)
-    req.session.flashMessage = {
-      status: "success",
-      txt: "Yes!! A new sneaker was created"
-    };
-  })
-  .catch(dbErr => {
-    console.log(dbErr)
-  })
+  const { name, ref, size, description, category, price, tags } = req.body;
+
+  const newProduct = { name, ref, size, category, description, price, tags };
+
+  if (req.file) {
+    newProduct.image = req.file.secure_url;
+  }
+
+  sneakerApi
+    .create(newProduct)
+    .then(dbRes => {
+
+      req.session.msg = {
+        status: "success",
+        txt: "Yes!! A new sneaker was created"
+      };
+
+      res.redirect("/prod-add");
+  
+    })
+    .catch(dbErr => {
+      console.log(dbErr);
+    });
 });
 
 router.get("/prod-manage", (req, res) => {
-  res.render("products_manage");
+  sneakerApi.getAll("collection")
+  .then(apiRes => {
+    console.log(apiRes)
+    res.render("products_manage", {
+      sneakers: apiRes,
+      scripts: ["ajax_dashboard_delete.js"]
+    });
+  })
 });
 
-router.get("/product-edit", (req, res) => {
-  res.render("product_edit");
+router.get("/product-edit/:id", (req, res) => {
+  Promise.all([sneakerApi.getOne(req.params.id), tagApi.getAll()])
+  .then(apiRes => {
+    console.log(apiRes)
+    res.render("product_edit", {sneaker: apiRes[0], tags: apiRes[1]})
+  })
+  .catch(apiErr => next(apiErr))
+});
+
+router.post("/prod-edit/:id", uploader.single("image"), (req, res) => {
+
+  const { name, ref, size, description, category, price, tags } = req.body;
+  const updatedProduct = { name, ref, size, category, description, price, tags };
+
+  if (req.file) updatedProduct.image = req.file.secure_url;
+
+  sneakerApi
+    .findByIdAndUpdate({_id: req.params.id}, updatedProduct)
+    .then(dbRes => {
+
+      req.session.msg = {
+        status: "success",
+        txt: "Yes!! Sneaker successfully updated"
+      };
+
+      res.redirect("/prod-manage");
+  
+    })
+    .catch(dbErr => {
+      console.log(dbErr);
+    });
 });
 
 
-module.exports = router  
+module.exports = router;
