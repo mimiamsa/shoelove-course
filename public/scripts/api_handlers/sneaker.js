d6b89748ef5a5cf3bf19bfcd1090789e57ee20f4
meta-one <meta-one@meta-one.local>
2019-06-29 16:51:36 +0200

    small changes ^^

--- public/scripts/api_handlers/sneaker.js
+++ public/scripts/api_handlers/sneaker.js
@@ -1,15 +1,21 @@
 const url = document.getElementById("site_url").getAttribute("data-url");
 
 const handler = axios.create({
-    baseURL: `${url}/api/sneaker`
+  baseURL: `${url}/api/sneaker`
 });
 
-const getProductsByTagId = (id) => handler.get(`/all/${id}`);
+const getProductsByTagId = (ids, cat) => {
+  var tagQueries = "";
+  ids.forEach((id, i) => {
+    tagQueries += `id_tag_${i}=${id}`;
+    if (ids.length > 1 && i < ids.length - 1) tagQueries += "&";
+  });
+  return handler.get(`/by-tags-ids?category=${cat}&` + tagQueries);
+};
 
-const destroy = (id) => handler.delete(`/${id}`);
-
+const destroy = id => handler.delete(`/${id}`);
 
 export default {
-    getProductsByTagId,
-    destroy
-}
+  getProductsByTagId,
+  destroy
+};
